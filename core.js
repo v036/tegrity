3163d396258f1fd48783bc8e8ead7684f885f45a
36 <102698287+v036@users.noreply.github.com>
2023-03-06 21:31:13 +0100

    Add files via upload

---
+++ core.js
@@ -0,0 +1,22 @@
+// Variables
+const http = new XMLHttpRequest();
+const title = document.getElementById("title");
+const favicon = document.getElementById("favicon")
+const wrapper = document.getElementById("wrapper");
+const discord = document.getElementById("discord");
+
+http.onload = function(context) {
+    var rawmetadata = context.target.responseText;
+    const metadata = JSON.parse(rawmetadata);
+    
+    title.innerText = metadata.home.title;
+    favicon.setAttribute("href", metadata.home.favicon);
+    discord.setAttribute("href", metadata.home.discord)
+}
+
+http.open("GET", "/meta.json", true);
+http.send();
+
+setTimeout(() => {
+    wrapper.setAttribute("class", "visible");
+}, 100)
